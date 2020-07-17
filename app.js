d60110c4a4a290316daf59fdecc0224d8eedda8e
mdwohl <mdwohl@gmail.com>
2020-07-16 18:06:41 -0700

    major JS reformat

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
-'use strict'
+'use strict';
 //Matt Petersen & I pair programmed a lot of this. He helped quite a bit with setup and troubleshooting.
+var storesPush = [];
+var globalTime = ['6am: ','7am: ','8am: ','9am: ','10am: ','11am: ','12pm: ','1pm: ','2pm: ','3pm: ','4pm: ','5pm: ','6pm: ','7pm: '];
 
 function Store(name, min, max, avgCookies) {
   this.name = name;
@@ -9,7 +11,7 @@ function Store(name, min, max, avgCookies) {
   this.totalCookies = 0;
   this.time = ['6am: ','7am: ','8am: ','9am: ','10am: ','11am: ','12pm: ','1pm: ','2pm: ','3pm: ','4pm: ','5pm: ','6pm: ','7pm: '];
   this.cookieStorage = [];
-  
+  storesPush.push(this);
 
   this.renderCityName = function(){
     var table = document.getElementById('tableNumbers');
@@ -29,7 +31,7 @@ function Store(name, min, max, avgCookies) {
     cookieTotalsAll.textContent = this.totalCookies;
     row.appendChild(cookieTotalsAll);
     table.appendChild(row);
-  }
+  };
   
   function getRandomArbitrary(min,max) {
     min = Math.ceil(min);
@@ -42,11 +44,11 @@ function Store(name, min, max, avgCookies) {
       var customerNumber = getRandomArbitrary(this.min, this.max);
       var cookiesThisHour = Math.round(customerNumber * this.avgCookies);
       // this.totalCookies += cookiesThisHour;
-      //Alistair helped me with line 28. Skyler helped me clean up and make sense of many of the individual methods.
+      //Alistair helped me with this. Skyler helped me clean up and make sense of many of the individual methods.
       this.cookieStorage.push(cookiesThisHour);
       this.totalCookies = cookiesThisHour + this.totalCookies;
     }
-  }
+  };
   this.renderTimeDisplay = function(){
     var table = document.getElementById('tableNumbers');
     var row = document.createElement('tr');
@@ -63,7 +65,7 @@ function Store(name, min, max, avgCookies) {
     tableData.textContent = 'City total: ';
     row.appendChild(tableData);
     table.appendChild(row);
-  }
+  };
   this.renderBottomRow = function(){
     var table = document.getElementById('tableNumbers');
     var row = document.createElement('tr');
@@ -83,8 +85,34 @@ function Store(name, min, max, avgCookies) {
     tableData.textContent = cookieCounter;
     row.appendChild(tableData);
     table.appendChild(row);
-  }
+
+    // var tableFootRender = document.getElementById('tableFootData');
+    var tableFoot = document.createElement('tfoot');
+    tableFoot.appendChild(row);
+    table.appendChild(tableFoot);
+    // tableFoot.textContent = ;
+    // var grandTotal = 0;
+    // for (var x = 0; x < this.time.length; x++){
+    //   var newTotal = 0;
+    //   for (var j = 0; j < storesPush.length; j++){
+    //     newTotal = newTotal + storesPush[j].cookieStorage[x];
+    //     tableData.textContent = newTotal;
+    //     row.appendChild(tableData);
+    //     grandTotal = grandTotal + newTotal;
+  };
 }
+// var tableFoot = document.createElement('tfoot');
+// tableFoot.appendChild(row);
+// table.appendChild(tableFoot);
+
+
+//1. set grand total var = 0
+//2. loop through hours of opeartion (line 75 does this)
+//3. declare a total 
+//4. create a loop with a different variable (j) to iterate through stores [j].cookiesPerHour[i].
+//5. give content; append to parent
+//6. grand total will equal grand total plus totals
+
 var store1 = new Store('Seattle', 23, 65, 6.5);
 
 store1.calculatePerHour();
@@ -111,3 +139,37 @@ var store5 = new Store('Lima', 2, 16, 4.6);
 store5.calculatePerHour();
 store5.renderCityName();
 store5.renderBottomRow();
+
+var grandTotal = 0;
+for (var i = 0; i < globalTime.length; i++){
+  var newTotal = 0;
+  for (var j = 0; j < storesPush.length; j++){
+    grandTotal = newTotal + storesPush[j].cookieStorage[i];
+    newTotal += storesPush[j] + cookieStorage[i];
+  }
+  var tableCell = document.createElement('td');
+  tableCell.textContent(newTotal);
+  row.appendChild(tableData);
+}
+var grandTotalCell = document.createElement('td');
+grandTotalCell.textContent = newTotal;
+row.appendChild(grandTotalCell);
+
+
+
+//below code is for the form
+var inputNewStoreForm = document.getElementById('makeNewStore');
+inputNewStoreForm.addEventListener('submit', makeNewStore);
+function makeNewStore(event) {
+  event.preventDefault();
+  console.log(event);
+
+  var newName = event.target.storeName.value;
+  var newMin = event.target.Min.value;
+  var newMax = event.target.Max.value;
+  var newAvgCookies = event.target.average.value;
+  var newStore = new Store(newName, newMin, newMax, newAvgCookies);
+  newStore.renderCityName();
+  console.log(newStore);
+}
+//render table here
---
+++ test.js
@@ -0,0 +1,163 @@
+'use strict';
+//Matt Petersen & I pair programmed a lot of this. He helped quite a bit with setup and troubleshooting.
+var storesPush = [];
+var time = ['6am: ','7am: ','8am: ','9am: ','10am: ','11am: ','12pm: ','1pm: ','2pm: ','3pm: ','4pm: ','5pm: ','6pm: ','7pm: '];
+
+var newStoreForm = document.getElementById('makeNewStore');
+makeNewStore.addEventListener('submit', addStoreToTable);
+
+function addStoreToTable(event){
+  event.preventDefault();
+  var newStoreName = event.target.storeName.value;
+  var minCust = event.target.min.value;
+  var maxCust = event.target.max.value;
+  var newStoreAverage = event.target.average.value;
+
+  var addNewStoreToTable = new newStoreFunction(minCust, maxCust, newStoreAverage, newStoreName);
+  addNewStoreToTable.renderAsTable();
+}
+//constructor begins here
+
+function NewStoreFunction(name, min, max, avgCookies) {
+  this.name = name;
+  this.min = min;
+  this.max = max;
+  this.avgCookies = avgCookies;
+  this.totalCookies = 0;
+  this.time = ['6am: ','7am: ','8am: ','9am: ','10am: ','11am: ','12pm: ','1pm: ','2pm: ','3pm: ','4pm: ','5pm: ','6pm: ','7pm: '];
+  this.cookieStorageArray = [];
+
+  storesPush.push(this);
+}
+NewStoreFunction.prototype.multiplyCustomersByCookieAverage = function() {
+  for(var i = 0; i < time.length; i++){
+    console.log('number calculation');
+    var cookies = this.multiplyCustomersByCookieAverage();
+    console.log(cookies);
+    this.cookieStorageArray.push(cookies);
+  }
+
+  //this prototype will calculate the sales per hour
+  NewStoreFunction.prototype.calculatePerHour = function() {
+    for(var i = 0; i < time.length; i++){
+      var cookies = this.multiplyCustomersByCookieAverage();
+      this.cookieStorageArray.push(cookies);
+    }
+  };
+
+
+  //This prototype will calculate the cookies per store
+  NewStoreFunction.prototype.calculatePerStore = function() {
+    for(var i = 0; i < this.time.length; i++){
+      this.totalCookies = this.totalCookies + this.cookieStorageArray[i];
+    }
+  };
+
+//This function will render the hours row at table top
+  function renderTimeDisplay(){
+    var table = document.getElementById('tableNumbers');
+    var row = document.createElement('tr');
+    var tableData = document.createElement('td');
+    row.appendChild(tableData);
+
+    for (var i = 0; i < time.length; i++){
+      var tableData = document.createElement('td');
+      tableData.textContent = time[i];
+      row.appendChild(tableData);
+    }
+    var tableData = document.createElement('td');
+    tableData.textContent = 'City total: ';
+    row.appendChild(tableData);
+    table.appendChild(row);
+  };
+
+  NewStoreFunction.prototype.dailyStoreTotal = function() {
+    for(var i = 0; i < this.time.length; i++){
+      this.totalCookies = this.totalCookies + this.cookieStorageArray[i];
+    }
+  };
+
+//city name plus the sales data
+
+
+NewStoreFunction.prototype.renderCityWithCookies.renderAsTable = function(){
+    var table = document.getElementById('tableNumbers');
+    var row = document.createElement('tr');
+    var tableData = document.createElement('td');
+    tableData.textContent = this.name;
+    row.appendChild(tableData);
+
+    this.calculatePerHour();
+    for(var i = 0; i < time.length; i++) {
+      var CookiesHourElement = document.createElement('td');
+      CookiesHourElement.textContent = this.cookieStorageArray[i];
+      row.appendChild(CookiesHourElement);
+    }
+    var cookieTotalsAll = document.createElement('td');
+    this.calculatePerStore();
+    cookieTotalsAll.textContent = this.totalCookies;
+    row.appendChild(cookieTotalsAll);
+    table.appendChild(row);
+  };
+  //totals on the bottom row for each hour
+  function renderTotalsBottomRow(){
+    var table = document.getElementById('tableNumbers');
+    var row = document.createElement('tr');
+    var tableData = document.createElement('td');
+    tableData.textContent = 'Hourly all locations';
+    row.appendChild(tableData);
+
+    var grandTotal = 0;
+    for (var i = 0; i < time.length; i++){
+      var newTotal = 0;
+      for (var j = 0; j < storesPush.length; j++){
+        grandTotal = newTotal + storesPush[j].cookieStorage[i];
+        newTotal += storesPush[j] + cookieStorage[i];
+      }
+      var tableCell = document.createElement('td');
+      tableCell.textContent(newTotal);
+      row.appendChild(tableData);
+    }
+    var grandTotalCell = document.createElement('td');
+    grandTotalCell.textContent = newTotal;
+    row.appendChild(grandTotalCell);
+  }
+  var tableData = document.createElement('tfoot');
+  tableData.textContent = grandTotal;
+  row.appendChild(tableData);
+  table.appendChild(row);
+
+  // var tableFoot = document.createElement('tfoot');
+  // tableFoot.appendChild(row);
+  // table.appendChild(tableFoot);
+};
+
+new NewStoreFunction('Seattle', 23, 65, 6.5);
+new NewStoreFunction('Tokyo', 3, 24, 1.2);
+new NewStoreFunction('Dubai', 11, 38, 3.7);
+new NewStoreFunction('Paris', 20, 38, 2.3);
+new NewStoreFunction('Lima', 2, 16, 4.6);
+
+renderTimeDisplay();
+for(var i = 0; i < storesPush.length; i++){
+  storesPush[i].renderAsTable();
+}
+
+renderTotalsBottomRow();
+
+
+// var inputNewStoreForm = document.getElementById('makeNewStore');
+// inputNewStoreForm.addEventListener('submit', makeNewStore);
+// function makeNewStore(event) {
+//   event.preventDefault();
+//   console.log(event);
+
+//   var newName = event.target.storeName.value;
+//   var newMin = event.target.Min.value;
+//   var newMax = event.target.Max.value;
+//   var newAvgCookies = event.target.average.value;
+//   var newStore = new Store(newName, newMin, newMax, newAvgCookies);
+//   newStore.renderCityName();
+//   console.log(newStore);
+// }
+//render table here
